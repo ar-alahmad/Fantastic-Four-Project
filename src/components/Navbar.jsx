4e90682a6d834ce5a3f7c0d550366639f9d19b84
Dmytro Kuzyk <dmytro.kuzyk@gmail.com>
2025-04-18 18:14:44 +0200

    basic routing + header + footer + pics

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,17 @@
-// Navbar.jsx
+import { NavLink } from "react-router-dom";
+
 export default function Navbar() {
-    return <nav>Navigation here</nav>
-  }
-  
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <ul className="nav-links">
+          <li><NavLink to="/accommodation">Accommodation</NavLink></li>
+          <li><NavLink to="/summer-holiday">Summer</NavLink></li>
+          <li><NavLink to="/"><img className="logo" src="/berliner-hotel-logo.jpg" alt="Hotel Logo" /></NavLink></li>
+          <li><NavLink to="/winter-holiday">Winter</NavLink></li>
+          <li><NavLink to="/contact">Contact</NavLink></li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
